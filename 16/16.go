caf07c9b512ebcb45d1ba7f810d3b971816d07a9
David Mašek <david.masek@blindspot.ai>
2023-11-09 18:57:17 +0100

    16b - nice version

--- 16/16.go
+++ 16/16.go
@@ -22,7 +22,7 @@ type Valve struct {
 	NeighborDistance map[string]int
 }
 
-const END_DAY = 30
+const END_DAY = 26
 const OPEN_TIME = 1
 
 type Node struct {
@@ -121,18 +121,97 @@ func solve(valvesArr []InputValve) {
 	}
 
 	// A* search
-	q := make(PriorityQueue, 0)
-	heap.Init(&q)
 	startingNode := &Node{
 		position:     "AA",
 		openedValves: make(map[string]bool, 0),
 	}
+	fmt.Println("Generating best solution for elf")
+	bestSolutionNode, openedCount := findBest(startingNode, valves, dist, maxFlow)
+	fmt.Println("T:", bestSolutionNode.day, "A:", bestSolutionNode.released)
+	fmt.Println("# Opened:", openedCount)
+
+	fmt.Println("Generating best complementary solution")
+	startingNode = &Node{
+		position:     "AA",
+		openedValves: make(map[string]bool, 0),
+	}
+	for k, v := range bestSolutionNode.openedValves {
+		startingNode.openedValves[k] = v
+	}
+	remainingSolutionNode, openedCount := findBest(startingNode, valves, dist, maxFlow)
+	fmt.Println("T:", remainingSolutionNode.day, "A:", remainingSolutionNode.released)
+	fmt.Println("# Opened:", openedCount)
+
+	// use remainingSolutionNode.released as heuristic (prune below) for new "exhaustive search"
+	fmt.Println("Generating all feasible solutions (ones better than the complementary one)")
+	startingNode = &Node{
+		position:     "AA",
+		openedValves: make(map[string]bool, 0),
+	}
+	allFeasible, openedCount := findAll(startingNode, valves, dist, maxFlow, remainingSolutionNode.released)
+	fmt.Println("# Found:", len(allFeasible))
+	fmt.Println("# Opened:", openedCount)
+
+	bestTotalReleased := 0
+	bestTotalSolution := &Node{}
+	bestTotalSolutionComplementary := &Node{}
+	for _, feasible := range allFeasible {
+		startingNode = &Node{
+			position:     "AA",
+			openedValves: make(map[string]bool, 0),
+		}
+		for k, v := range feasible.openedValves {
+			startingNode.openedValves[k] = v
+		}
+		bestComplementary, _ := findBest(startingNode, valves, dist, maxFlow)
+
+		totalReleased := feasible.released + bestComplementary.released
+		if totalReleased > bestTotalReleased {
+			bestTotalReleased = totalReleased
+			bestTotalSolution = feasible
+			bestTotalSolutionComplementary = bestComplementary
+		}
+	}
+	fmt.Println("Best total solution:", bestTotalReleased)
+	fmt.Println(bestTotalSolution, bestTotalSolutionComplementary)
+}
+
+func findAll(startingNode *Node, valves map[string]Valve, dist map[string]map[string]int, maxFlow int, threshold int) ([]*Node, int) {
+	q := make([]*Node, 0)
+	q = append(q, startingNode)
+	solutions := make([]*Node, 0)
+	openedCount := 0
+
+	for len(q) > 0 {
+		openedCount++
+		currentNode := q[0]
+		q = q[1:]
+
+		nbs := generateNeighbors(currentNode, valves, dist, maxFlow)
+		for _, node := range nbs {
+			// if actual release is above threshold, it is feasible solution
+			// -> save it
+			if node.released > threshold {
+				solutions = append(solutions, node)
+			}
+			// if priority is above threshold, it is possible this will be feasible solution
+			// -> continue processing
+			if node.priority > threshold {
+				q = append(q, node)
+			}
+		}
+	}
+	return solutions, openedCount
+}
+
+func findBest(startingNode *Node, valves map[string]Valve, dist map[string]map[string]int, maxFlow int) (*Node, int) {
+	q := make(PriorityQueue, 0)
+	heap.Init(&q)
 	heap.Push(&q, startingNode)
 
 	bestSolution := 0
 	bestSolutionNode := &Node{}
 	openedCount := 0
-	solutions := make([]*Node, 0)
 
 	for len(q) > 0 {
 		openedCount++
@@ -150,9 +229,7 @@ func solve(valvesArr []InputValve) {
 			}
 		}
 	}
-	fmt.Println("T:", bestSolutionNode.day, "A:", bestSolution)
-	fmt.Println("# Opened:", openedCount)
-	fmt.Println("# Solutions:", len(solutions))
+	return bestSolutionNode, openedCount
 }
 
 func generateNeighbors(currentNode *Node, valves map[string]Valve, dist map[string]map[string]int, maxFlow int) []*Node {
@@ -214,6 +291,10 @@ func generateNeighbors(currentNode *Node, valves map[string]Valve, dist map[stri
 			released:    totalReleased,
 			day:         END_DAY,
 			priority:    totalReleased,
+		}
+		n.openedValves = make(map[string]bool)
+		for k, v := range currentNode.openedValves {
+			n.openedValves[k] = v
 		}
 		nbs = append(nbs, n)
 	}
