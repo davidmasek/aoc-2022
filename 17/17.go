4d21fd5afa45830dcfb3a18c6eaee681bf40c422
David Mašek <david.masek@blindspot.ai>
2023-11-09 20:39:22 +0100

    17b - messy cycle detection

--- 17/17.go
+++ 17/17.go
@@ -164,6 +164,15 @@ func (rock *Rock) isBlockedAt(shift Point, pile map[Point]bool, chamberWidth int
 	return false
 }
 
+type State struct {
+	windIndex        int
+	nextRockIndex    int
+	occupiedBitMap   []bool
+	round            int
+	height           int
+	fallenRocksCount int
+}
+
 func solve(winds string) {
 	const CHAMBER_WIDTH = 7
 	// start with left edge this from left edge of chamber
@@ -175,7 +184,12 @@ func solve(winds string) {
 	rocks = append(rocks, rock)
 	pile := make(map[Point]bool)
 	// draw(rocks)
+
+	stateLibrary := make([]State, 0)
+
 	fallenCounter := 0
+	// const TARGET = 2022
+	const TARGET = 1000000000000
 	for round := 0; ; round++ {
 		windIndex := round % len(winds)
 		wind := winds[windIndex]
@@ -196,21 +210,95 @@ func solve(winds string) {
 		blockedBelow := rock.isBlockedAt(Point{0, -1}, pile, CHAMBER_WIDTH)
 		if blockedBelow {
 			// fmt.Println("--- NEW ROCK ---")
+			// Solidify rock
 			for _, p := range rock.blocks {
 				pile[Point{rock.position.x + p.x, rock.position.y + p.y}] = true
 			}
+			fallenCounter++
+			nextRockIndex := fallenCounter % 5
+			// maximum occupied point
 			startY := 0
 			for _, rock := range rocks {
 				startY = max(startY, rock.position.y)
 			}
+			// state depends on:
+			// (1) wind index
+			// (2) next rock index
+			// (3) some state of the blockage
+			const DEPTH_SEARCHED = 60 // magic - increase until the results stop changing
+			occupiedBitMap := make([]bool, CHAMBER_WIDTH*DEPTH_SEARCHED)
+			for dy := 0; dy < DEPTH_SEARCHED; dy++ {
+				for x := 0; x < CHAMBER_WIDTH; x++ {
+					_, occupied := pile[Point{x, startY - dy}]
+					occupiedBitMap = append(occupiedBitMap, occupied)
+				}
+			}
+			currentState := State{
+				windIndex:        windIndex,
+				nextRockIndex:    nextRockIndex,
+				occupiedBitMap:   occupiedBitMap,
+				round:            round,
+				height:           startY + 1,
+				fallenRocksCount: fallenCounter,
+			}
+		outer:
+			for i, s := range stateLibrary {
+				if s.nextRockIndex != currentState.nextRockIndex ||
+					s.windIndex != currentState.windIndex {
+					continue
+				}
+				for i := 0; i < len(s.occupiedBitMap); i++ {
+					if s.occupiedBitMap[i] != currentState.occupiedBitMap[i] {
+						continue outer
+					}
+				}
+				fmt.Println("FOUND CYCLE!")
+				fmt.Println("Rounds:", currentState.round, s.round)
+				fmt.Println("Round diff", currentState.round-s.round)
+				fmt.Println("Heights:", currentState.height, s.height)
+				fmt.Println("Height diff", currentState.height-s.height)
+				fmt.Println("# Fallen", currentState.fallenRocksCount, s.fallenRocksCount)
+				fmt.Println("Fallen diff", currentState.fallenRocksCount-s.fallenRocksCount)
+
+				// po 15 spadlych je vyska 25
+				// heightAtCycleStart := s.height
+				// pak za kazdych 35 spadlych vzroste o 53
+				cycleInterval := currentState.fallenRocksCount - s.fallenRocksCount
+				cycleHeightIncrease := currentState.height - s.height
+				// nas zajima stav po 2022 spadlych
+				// 2022 - 15 = 2007
+				tillTarget := TARGET - s.fallenRocksCount
+				// 2007 / 35 ~= 57
+				wholeCyclesTillTarget := tillTarget / cycleInterval
+				// 57 * 53 = 3021
+				increaseDuringCycles := wholeCyclesTillTarget * cycleHeightIncrease
+				// po 15 + 35 * 57 = 2010 je vyska:
+				// 3021 + 25 = 3046
+				// heightAfterCycles := heightAtCycleStart + increaseDuringCycles
+				// zbyva 2022 - 2010 = 12
+				// (pripadne 2007 % 35 = 12)
+				// (oboji je rozdil co nam jeste zbyva)
+				remainingRocksAfterCycles := tillTarget % cycleInterval
+				// po 12 spadlych vyska vzroste o 21 (ze simulace, z historie)
+				fmt.Println("---")
+				fmt.Println("Simulate extra #:", remainingRocksAfterCycles)
+				// celkem tedy
+				// 3046 + 21 = 3067
+				fmt.Println("Simulate extra # after height:", stateLibrary[i+remainingRocksAfterCycles].height)
+				fmt.Println("Increase during cycles:", increaseDuringCycles)
+				fmt.Println("Total", increaseDuringCycles+stateLibrary[i+remainingRocksAfterCycles].height)
+				return
+			}
+			stateLibrary = append(stateLibrary, currentState)
+
+			// Create new rock
 			startY += START_DY
-			fallenCounter++
-			if fallenCounter == 2022 {
-				// if fallenCounter == 10 {
+			if fallenCounter == TARGET {
+				// if fallenCounter == 254 {
 				fmt.Println("A:", startY-START_DY+1)
 				break
 			}
-			switch fallenCounter % 5 {
+			switch nextRockIndex {
 			case 0:
 				rock = RockMinus(Point{START_DX, startY})
 			case 1:
